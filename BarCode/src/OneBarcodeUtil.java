ec92d6b92e356c206c65df33501165f0716bea04
Tom Xue <tomxue@outlook.com>
2013-09-25 16:27:24 +0800

    to show how to generate Bar Code

---
+++ BarCode/src/OneBarcodeUtil.java
@@ -0,0 +1,39 @@
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+
+import org.jbarcode.JBarcode;
+import org.jbarcode.encode.EAN8Encoder;
+import org.jbarcode.paint.EAN8TextPainter;
+import org.jbarcode.paint.WidthCodedPainter;
+import org.jbarcode.util.ImageUtil;
+
+public class OneBarcodeUtil { 
+ 
+    public static void main(String[] paramArrayOfString) { 
+        try { 
+            JBarcode localJBarcode = new JBarcode(EAN8Encoder.getInstance(),WidthCodedPainter.getInstance(),EAN8TextPainter.getInstance()); 
+            String str = "3126456"; 
+            BufferedImage localBufferedImage = localJBarcode.createBarcode(str); 
+             
+            saveToJPEG(localBufferedImage, "EAN8.jpg");
+        } 
+        catch (Exception localException) { 
+            localException.printStackTrace(); 
+        } 
+    } 
+ 
+    static void saveToJPEG(BufferedImage paramBufferedImage, String paramString) { 
+        saveToFile(paramBufferedImage, paramString, "jpeg"); 
+    } 
+ 
+    static void saveToFile(BufferedImage paramBufferedImage, String paramString1, String paramString2) { 
+        try { 
+            FileOutputStream localFileOutputStream = new FileOutputStream("C:\\Users\\xuejd1\\Desktop/" + paramString1); 
+            ImageUtil.encodeAndWrite(paramBufferedImage, paramString2, localFileOutputStream, 96, 96); 
+            localFileOutputStream.close(); 
+        } 
+        catch (Exception localException) { 
+            localException.printStackTrace(); 
+        } 
+    } 
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Java_BarCode
 ============
 
 to generate Bar Code by Java
+
+The 1st committed code is from link below:
+http://my.oschina.net/jgy/blog/59937
